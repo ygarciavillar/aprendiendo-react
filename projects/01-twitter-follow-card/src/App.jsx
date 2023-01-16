52fb4c0d3b8a7a260b5c1419157d0e676a0badda
Yoandri Garcia Villar <ygarciavillar@gmail.com>
2023-01-16 15:29:35 -0500

    create the twitter card

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" alt="avatar midu" />
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
